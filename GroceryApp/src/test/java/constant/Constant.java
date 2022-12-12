bc46e4f780b584edcf5b3aed1af8d4fa1c793ca1
user <user@DESKTOP-0F5314O.bbrouter>
2022-12-12 14:56:02 +0530

    edited

---
+++ GroceryApp/src/test/java/constant/Constant.java
@@ -0,0 +1,11 @@
+package constant;
+
+public class Constant 
+{
+// value doest change bcs final, approach to avoid hardcoding
+	//capality / features of the project
+	public static final String errormsgInvalidLogin="Button value not same";
+    public static final String invalidLoginErrorMsg ="Username and password is Invalid";
+    public static final String errorMessage="Actual and Expected is not same";
+    public static final String nullMessage="The field is Not Null:::";
+}
